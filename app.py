2575557a26c4473092c6a850f2c4b9f005a0785d
Dan Nguyen <ndan0112@gmail.com>
2023-05-19 15:43:46 -0500

    webapp works

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, jsonify
 import subprocess
+import threading
 
 import InputOutput
 import speech_recognition
+import ChatGPT
+import text_to_speech
+
 
 app = Flask(__name__)
 
@@ -15,7 +19,22 @@ def start_recording():
     # Call the separate Python file to stop audio recording and process the recorded audio
     result = speech_recognition.recognize_from_microphone()
     print(result)
-    return result
+    return jsonify({'status': 'success', 'userInput': result})
+
+@app.route('/process-text', methods=['POST'])
+def process_text():
+    data = request.json
+    text = data.get('user_input')
+    response = ChatGPT.chat_gpt(text)
+
+    def perform_text_to_speech():
+        audio_data = text_to_speech.text_to_speech(response)
+        # Code to save or send the audio data as needed
+
+    thread = threading.Thread(target=perform_text_to_speech)
+    thread.start()
+
+    return jsonify({'status': 'success', 'userInput': response})
 
 
 if __name__ == '__main__':
